469bb921a528e6999d6e6c31d38ceafbdb4a90bf
alex <lovejavaee@126.com>
2018-03-21 23:36:37 +0800

    initial commit

---
+++ src/main/java/com/example/mvcwithlatency/MvcWithLatencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.mvcwithlatency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcWithLatencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcWithLatencyApplication.class, args);
+	}
+}
